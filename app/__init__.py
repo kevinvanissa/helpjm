487cc14aeac485dabb2f695672e34318cd8c83e1
Kevin Miller <kevinvanissa@gmail.com>
2013-12-02 17:14:39 -0500

    entered loggingAgain

--- app/__init__.py
+++ app/__init__.py
@@ -32,7 +32,7 @@ mail = Mail(app)
 if not app.debug:
     import logging
     from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler = RotatingFileHandler('/var/tmp/helpjm.log', 'a', 1 * 1024 * 1024, 10)
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     app.logger.addHandler(file_handler)
